153ebe1abdb0b37f9a2c2acc3a14fb1c71c31b63
apadki <apadki@users.noreply.github.com>
2019-03-30 13:26:53 -0400

    Basic Offer Service

--- neighborhood/src/test/java/com/example/nhood/offers/OfferServiceTest.java
+++ neighborhood/src/test/java/com/example/nhood/offers/OfferServiceTest.java
@@ -1,17 +1,24 @@
 package com.example.nhood.offers;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.atMost;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.test.util.ReflectionTestUtils;
 import com.example.nhood.domain.Offer;
 import com.example.nhood.entities.OfferEntity;
 import com.example.nhood.mappers.OfferMapper;
@@ -28,6 +35,7 @@ public class OfferServiceTest {
   @Mock
   private OfferRepository offerRepository;
    
+  @Mock
   OfferMapper offerMapper = new OfferMapperImpl();
   
   @Before
@@ -45,12 +53,22 @@ public class OfferServiceTest {
    entity.setOfferType("s");
    entity.setUserId(1);
    offers.add(entity);
-   ReflectionTestUtils.setField(offerService, "offerMapper", offerMapper);
    when(offerRepository.findFromUserEmail(ArgumentMatchers.anyString())).thenReturn(offers);   
    
    List<Offer> offersFromService = offerService.getOffersByEmail("ABC");
-   assertTrue(null, offersFromService.size() > 0);
-  
+   verify(offerRepository, times(1)).findFromUserEmail(ArgumentMatchers.anyString());
+   verify(offerRepository, atLeastOnce()).findFromUserEmail(ArgumentMatchers.anyString());
+   verify(offerRepository, atLeast(1)).findFromUserEmail(ArgumentMatchers.anyString());
+   verify(offerRepository, atMost(1)).findFromUserEmail(ArgumentMatchers.anyString());
+   verify(offerRepository, never()).findByUserId(ArgumentMatchers.anyInt());
+   
+   ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+   verify(offerRepository).findFromUserEmail(captor.capture());
+   
+   assertEquals("ABC", captor.getValue());
+   
+   assertTrue(offersFromService.size() > 0);
+   
   }
 
 }
